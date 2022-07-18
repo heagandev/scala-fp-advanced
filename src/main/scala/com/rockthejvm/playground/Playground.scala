08c42b768899e95c1ccf16cf780f9f7125ff8cce
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-07-18 06:30:40 -0400

    Test playground

--- src/main/scala/com/rockthejvm/playground/Playground.scala
+++ src/main/scala/com/rockthejvm/playground/Playground.scala
@@ -2,6 +2,6 @@ package com.rockthejvm.playground
 
 object Playground {
   def main(args: Array[String]): Unit = {
-    
+    println("Up and running! Looking forward to getting advanced with Scala!")
   }
 }
